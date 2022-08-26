ddcaa52ee4a56efcdb6d782edc496593e1246ee8
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-26 08:23:31 +0300

    PW

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -175,12 +175,10 @@ getContent(token) {
 }
 }
 
-
-/* Здесь создаем экземпляр класса Api и экспортируем этот экземпляр вместо самого класса
 export const Auth = new Api({
 baseUrl: "https://auth.nomoreparties.co",
 headers: { "Content-Type": "application/json" },
-});*/
+});
 
 
 // Здесь создаем экземпляр класса Api с нужными параметрами, включая токен, и экспортируем этот экземпляр вместо самого класса
