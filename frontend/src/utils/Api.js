80830a7380d626d18ed4d4904cde4ffe77ff0bb2
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-27 09:21:20 +0300

    1

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,9 +1,17 @@
 import {cohort, token} from './Utils';
 
 export class Api {
-  constructor(options) {
-    this._baseUrl = "https://backend.mesto.tinaevnk.nomoredomains.xyz";
-    this._headers = options.headers;
+  constructor({baseUrl, headers}) {
+    this._url = baseUrl;
+    this._headers = headers;
+  }
+
+  _getHeaders() {
+    const jwt = localStorage.getItem('jwt');
+    return {
+      'Authorization': `Bearer ${jwt}`,
+      ...this._headers,
+    };
   }
 
   //Ответ от сервера
@@ -18,10 +26,9 @@ export class Api {
   // Публичный метод для загрузки пользовательского профиля
 
   getUserProfile() {
-    const request = this._baseUrl + "/users/me";
-    return fetch(request, {
+    return fetch(`${this._url}/users/me`, {
       method: "GET",
-      headers: this._headers,
+      headers: this._getHeaders(),
     })
       .then((res) => this._checkResponse(res))
   }
@@ -29,21 +36,18 @@ export class Api {
   //Публичный метод для загрузки карточек
 
   getInitialCards() {
-    const request = this._baseUrl + "/cards";
-    // возвращаем промис
-    return fetch(request, {
+    return fetch(`${this._url}/cards`, {
       method: "GET",
-      headers: this._headers,
+      headers: this._getHeaders(),
     })
       .then((res) => this._checkResponse(res))
   }
 
   // Метод для удаления карточки
   deleteCard(cardId) {
-    const request = this._baseUrl + `/cards/${cardId}`;
-    return fetch(request, {
+    return fetch(`${this._url}/cards/${cardId._id}`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders(),
     })
       .then((res) => this._checkResponse(res))
   }
@@ -51,12 +55,9 @@ export class Api {
 
   // Метод для добавления карточки
   addCard(cardData) { 
-    const request = this._baseUrl + "/cards";
-    const newHeaders = this._headers;
-    newHeaders["Content-Type"] = "application/json";
-    return fetch(request, {
+    return fetch(`${this._url}/cards`, {
       method: "POST",
-      headers: newHeaders,
+      headers: this._getHeaders(),
       body: JSON.stringify({
         name: cardData.name,
         link: cardData.link,
@@ -68,13 +69,9 @@ export class Api {
   //Метод для сохранения данных профиля 
 
   saveNewProfile(profileData) {
-    const request = this._baseUrl + "/users/me";
-    const newHeaders = this._headers;
-    newHeaders["Content-Type"] = "application/json";
-    // отправляем запрос
-    return fetch(request, {
+    return fetch(`${this._url}/users/me`, {
       method: "PATCH",
-      headers: newHeaders,
+      headers: this._getHeaders(),
       body: JSON.stringify({
         name: profileData.name,
         about: profileData.about,
@@ -85,12 +82,9 @@ export class Api {
 
   // Метод для обновления автара 
   updateAvatar(newAvatar) {
-    const request = this._baseUrl + "/users/me/avatar";
-    const newHeaders = this._headers;
-    newHeaders["Content-Type"] = "application/json";
-    return fetch(request, {
+    return fetch(`${this._url}/users/me/avatar`, {
       method: "PATCH",
-      headers: newHeaders,
+      headers: this._getHeaders(),
       body: JSON.stringify({
         avatar: newAvatar.link,
       }),
@@ -100,20 +94,18 @@ export class Api {
 
   // Метод для лайка карточки
   likeCard(cardId) {
-    const request = this._baseUrl + `/cards/${cardId}/likes`;
-    return fetch(request, {
+    return fetch(`${this._url}/cards/${cardId._id}/likes`, {
       method: "PUT",
-      headers: this._headers,
+      headers: this._getHeaders(),
     })
       .then((res) => this._checkResponse(res))
   }
 
   // Метод для удаления лайка 
   deleteLike(cardId) {
-    const request = this._baseUrl + `/cards/${cardId}/likes`;
-    return fetch(request, {
+    return fetch(`${this._url}/cards/${cardId._id}/likes`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders(),
     })
       .then((res) => this._checkResponse(res))
   }
@@ -128,10 +120,8 @@ export class Api {
   // создание пользователя
 
  register(password, email) {
-  const request = this._baseUrl + "/signup";
-
   return (
-    fetch(request, {
+    fetch(`${this._url}/signup`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({ password, email }),
@@ -143,16 +133,12 @@ export class Api {
 // авторизация 
 
 login(password, email) {
-  const request = this._baseUrl + "/signin";
-
-  return (
-    fetch(request, {
+    fetch(`${BASE_URL}/signin`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({ password, email }),
     })
-      .then((res) => this._checkResponse(res))
-  );
+      .then((res) => this._checkResponse(res));
 }
 
 // проверка токена
@@ -165,7 +151,7 @@ getContent(token) {
   return (
     fetch(request, {
       method: "GET",
-      headers: newHeaders,
+      headers: _getHeaders(),
     })
       .then((res) => this._checkResponse(res))
       .then((data) => {
@@ -184,7 +170,7 @@ headers: { "Content-Type": "application/json" },
 // Здесь создаем экземпляр класса Api с нужными параметрами, включая токен, и экспортируем этот экземпляр вместо самого класса
 export const api = new Api({
   baseUrl: `https://mesto.back.project.nomoredomains.sbs`,
-  headers: { authorization: token  }
+  headers: { 'Content-Type': 'application/json'  }
 });
 
 
