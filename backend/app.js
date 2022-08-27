77701a00ae4d6f722473700daa7fe3cb224290e4
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-27 11:41:09 +0300

    0

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,37 @@
+require('dotenv').config();
 const express = require('express');
+
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors, celebrate, Joi } = require('celebrate');
+const bodyParser = require('body-parser');
+
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const errorRouter = require('./routes/error');
 const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
 const { createUser, login } = require('./controllers/users');
+const { requestLogger, errorLoger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(requestLogger);
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-
-app.use(express.json());
 
 app.post(
   '/signin',
@@ -44,15 +61,10 @@ app.post(
 app.use('/', auth, usersRouter);
 app.use('/', auth, cardsRouter);
 app.all('*', auth, errorRouter);
+app.use(errorLoger);
+
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка на сервере' : message });
-  next();
-});
+app.use(errorHandler);
 
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
