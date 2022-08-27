29645626a9a001859e0c1df8d930ff7770e68fc7
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-27 20:12:44 +0300

    1

--- backend/routes/error.js
+++
@@ -1,8 +0,0 @@
-const errorRouter = require('express').Router();
-const NotFoundError = require('../errors/NotFoundError');
-
-errorRouter.all('*', (req, res, next) => {
-  next(new NotFoundError('Ресурс не найден'));
-});
-
-module.exports = errorRouter;
