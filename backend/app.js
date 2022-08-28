deb61ff934750d9ac57c9d1f1f1b58c7d77e4571
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-28 23:38:04 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,8 @@ app.get('/crash-test', () => {
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(express.json());
