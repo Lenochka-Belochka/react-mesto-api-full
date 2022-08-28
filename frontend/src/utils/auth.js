8eb69c9cb1b5238f1cebdf35f80295bdbd43715f
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-28 09:58:19 +0300

    1

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://mesto-dankuzmenko.herokuapp.com';
+export const BASE_URL = 'https://mesto.back.project.nomoredomains.sbs';
 
 const getJson = (response) => {
     if (response.ok) {
