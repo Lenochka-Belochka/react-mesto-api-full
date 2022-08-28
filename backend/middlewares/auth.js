cab9f2cc8a775ae2a5ef0e7a253cb41d3c1a5322
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-28 10:35:08 +0300

    0

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthError('Необходима авторизация');
   }
-  const token = authorization.replace('Bearer ', '');
+  const token = String(req.headers.authorization).replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
