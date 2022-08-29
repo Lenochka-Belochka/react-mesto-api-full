57dd28f5bb683a9b596634a7e9a7eaf4bbb9c946
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-30 01:23:35 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,7 @@ const { ERROR_SERVER } = require('./utils/const');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
