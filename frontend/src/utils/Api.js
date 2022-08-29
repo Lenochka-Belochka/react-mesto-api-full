a89b9496a3e6ffe8a2adf33c086d6b7e8789a294
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-29 21:43:15 +0300

    q

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,91 +1,103 @@
 class Api {
-	constructor(options) {
-		this._baseUrl = options.baseUrl;
-		this._headers = options.headers;
-	}
-	_checkServerStatus(res) {
-		if (res.ok) {
-			return res.json()
-		} else {
-			return Promise.reject(`Ошибка: ${res.status}`);
-		}
-	}
-	getUserInfo() {
-		return fetch(`${this._baseUrl}/users/me`, {
-			method: 'GET',
-			headers: this._headers,
-		}).then(this._checkServerStatus)
-	}
-	getInitialCards() {
-		return fetch(`${this._baseUrl}/cards`, {
-			method: 'GET',
-			headers: this._headers,
-		}).then(this._checkServerStatus)
-	}
-	getInitialInfo() {
-		return Promise.all([this.getUserInfo(), this.getInitialCards()])
-	}
-	setUserInfo(data) {
-		return fetch(`${this._baseUrl}/users/me`, {
-			method: 'PATCH',
-			headers: this._headers,
-			body: JSON.stringify({
-				name: data.name,
-				about: data.about
-			})
-		}).then(this._checkServerStatus)
-	}
-	updateAvatar(link) {
-		return fetch(`${this._baseUrl}/users/me/avatar`, {
-			method: 'PATCH',
-			headers: this._headers,
-			body: JSON.stringify({
-				avatar: link
-			})
-		}).then(this._checkServerStatus)
-	}
-	postNewCard({ name, link }) {
-		return fetch(`${this._baseUrl}/cards`, {
-			method: "POST",
-			headers: this._headers,
-			body: JSON.stringify({
+    constructor({ address }) {
+      this.address = address
+      }
+  
+      _checkServerStatus(res) {
+        return res.ok ? res.json() : Promise.reject(new Error(`Ошибка: ${res.status}`));
+      }
+  
+      getInitialCards() {
+          return fetch(`${this.address}/cards`, {
+            headers: {
+              authorization: this.getToken(),
+            },
+          })
+              .then(this._checkServerStatus);
+      }
+  
+      getUserInfo() {
+          return fetch(`${this.address}/users/me`, {
+            headers: {
+              authorization: this.getToken(),
+            },
+          })
+              .then(this._checkServerStatus)
+      }
+  
+      setUserInfo(data) {
+          return fetch(`${this.address}/users/me`, {
+              method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: this.getToken(),
+            },
+              body: JSON.stringify({
+                  name: data.name,
+                  about: data.about
+              })
+          })
+              .then(this._checkServerStatus)
+      }
+  
+      postNewCard({ name, link }) {
+          return fetch(`${this.address}/cards`, {
+              method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: this.getToken(),
+            },
+              body: JSON.stringify({
 				name,
 				link
-			})
-		}).then(this._checkServerStatus)
-	}
-	changeLikeCardStatus(id, isLiked) {
-		if (isLiked) {
-			return this.removeLike(id);
-		} else {
-			return this.addLike(id);
-		}
-	}
-	addLike(id) {
-		return fetch(`${this._baseUrl}/cards/${id}/likes`, {
-			method: "PUT",
-			headers: this._headers
-		}).then(this._checkServerStatus)
-	}
-	removeLike(id) {
-		return fetch(`${this._baseUrl}/cards/${id}/likes`, {
-			method: "DELETE",
-			headers: this._headers
-		}).then(this._checkServerStatus)
-	}
-	deleteCard(id) {
-		return fetch(`${this._baseUrl}/cards/${id}`, {
-			method: "DELETE",
-			headers: this._headers
-		}).then(this._checkServerStatus)
-	}
-}
-
-export const api = new Api({
-	baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-41',
-	headers: {
-		authorization: '0e0878b0-27c7-481c-9d47-0d05d423b137',
-		'Content-Type': 'application/json'
-	}
-});
-
+              })
+          })
+              .then(this._checkServerStatus)
+      }
+  
+      deleteCard(id) {
+          return fetch(`${this.address}/cards/${id}`, {
+              method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: this.getToken(),
+            },
+          })
+              .then(this._checkServerStatus)
+      }
+  
+      changeLikeCardStatus(id, isLiked) {
+          const method = isLiked ? 'DELETE' : 'PUT';
+          return fetch(`${this.address}/cards/${id}/likes`, {
+              method,
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: this.getToken(),
+            },
+          })
+              .then(this._checkServerStatus)
+      }
+  
+      updateAvatar(link) {
+          return fetch(`${this.address}/users/me/avatar`, {
+              method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: this.getToken(),
+            },
+              body: JSON.stringify({
+                  avatar: link
+              })
+          })
+            .then(this._checkServerStatus)
+      }
+  
+    getToken() {
+      return `Bearer ${localStorage.getItem('token')}`
+    }
+  }
+  
+  export const api = new Api({
+    address: 'https://mesto.back.project.nomoredomains.sbs',
+  });
+  
