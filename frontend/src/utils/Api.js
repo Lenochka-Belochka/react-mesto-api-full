29d6277d44cf94f1e152d249e23514a3815bd406
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-31 14:20:56 +0300

    origin

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -188,7 +188,7 @@ export const api = new Api({
   baseUrl: `https://mesto.back.project.nomoredomains.sbs`,
   headers: { 
   authorization: `Bearer ${localStorage.getItem('jwt')}`,
-  'Content-Type': 'application/json'
+  "Content-Type": "application/json"
  },
 });
 
