b19ddf3238dd98682eb0c8556a37069d465c56f5
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-31 21:25:39 +0300

    1

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -11,30 +11,29 @@ export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem('jwt')}`,
     },
     body: JSON.stringify({ email, password }),
   }).then(checkResponse);
 };
+
 
 export const login = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem('jwt')}`,
     },
     body: JSON.stringify({ email, password }),
   }).then(checkResponse);
 };
 
+
 export const getContent = () => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem('jwt')}`,
     },
   }).then(checkResponse);
 };
