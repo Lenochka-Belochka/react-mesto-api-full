69b0bfb923750e3d37b39e12d3fbe9e512312368
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-31 22:47:49 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(cookieParser());
 app.use(requestLogger);
 
